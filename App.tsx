3c3855cce7802a4af6143e9df3b6d2b3209bdfd6
Pavel Sušický <susicky.pavel@protonmail.com>
2023-03-03 22:57:06 +0100

    feat(rneui): add ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,25 +1,25 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "@rneui/base";
+import { View } from "react-native";
+
+import { ThemeProvider, createTheme } from "@rneui/themed";
+
 import { TestComponent } from "./src/components/test-component";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <Text>Má to fast refresh, cool 😎</Text>
-      <Button>Click</Button>
-      <TestComponent />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const theme = createTheme({
+  components: {
+    Button: {
+      raised: false,
+    },
   },
 });
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <View>
+        <TestComponent />
+      </View>
+    </ThemeProvider>
+  );
+};
+
+export default App;
