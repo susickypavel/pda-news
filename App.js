5c0ab1954c210343fa4c842799cc8d3831e236ab
Pavel Sušický <susicky.pavel@protonmail.com>
2023-03-03 22:32:44 +0100

    chore(deps): add rneui

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "@rneui/base";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
       <Text>Má to fast refresh, cool 😎</Text>
+      <Button>Click</Button>
       <StatusBar style="auto" />
     </View>
   );
