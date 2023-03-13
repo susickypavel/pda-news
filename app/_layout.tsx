28d660988d1d1c7aae3e2eccb6f44d4d6b5a07d9
Pavel Sušický <me@frontendista.cz>
2023-03-13 22:06:33 +0100

    feat(routing): add root layout and home page

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "@rneui/themed";
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function DefaultLayout() {
+	return (
+		<ThemeProvider>
+			<Stack />
+		</ThemeProvider>
+	);
+}
