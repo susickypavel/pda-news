c904351a166fb7240d6b01c35cdef10c1dca8324
Pavel Sušický <susicky.pavel@protonmail.com>
2023-03-14 12:10:22 +0100

    chore(build): add absolute imports

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,12 @@ module.exports = function (api) {
 
 	return {
 		presets: ["babel-preset-expo"],
-		plugins: [require.resolve("expo-router/babel")]
+		plugins: [
+			require.resolve("expo-router/babel"),
+			[
+				"module-resolver",
+				{ alias: { "@/components": "./src/components", "@/screens": "./src/screens", "@/api": "./src/api" } }
+			]
+		]
 	};
 };
--- src/screens/login-screen.tsx
+++ src/screens/login-screen.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-import { LoginForm } from "../components/login-form";
+import { LoginForm } from "@/components/login-form";
 
 export const LoginScreen: React.FC = () => {
 	return (
