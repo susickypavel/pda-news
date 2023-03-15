9587107f62ce5b82b4f0748c91a7252ab3002d3c
Pavel Sušický <me@frontendista.cz>
2023-03-15 10:08:30 +0100

    refactor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,38 @@
 import { ThemeProvider } from "@rneui/themed";
-import { Stack } from "expo-router";
-import React, { useEffect } from "react";
+import type { Session } from "@supabase/supabase-js";
+import { Stack, useRouter } from "expo-router";
+import React, { useEffect, useState } from "react";
 
 import { supabase } from "@/api/supabase";
 
 export default function DefaultLayout() {
+	const [authSession, setAuthSession] = useState<Session | null>(null);
+	const router = useRouter();
+
 	useEffect(() => {
-		supabase.auth.getSession().then(({ data: { session } }) => {
-			console.log(session);
+		supabase.auth
+			.getSession()
+			.then(({ data }) => {
+				setAuthSession(data.session);
+			})
+			.catch(reason => {
+				console.log(`ERROR: Couldn't fetch session status. (${reason})`);
+			});
+
+		const auth = supabase.auth.onAuthStateChange((_, session) => {
+			setAuthSession(session);
 		});
 
-		supabase.auth.onAuthStateChange((event, session) => {
-			console.log(event, session);
-		});
+		return () => auth.data.subscription.unsubscribe();
 	}, []);
+
+	useEffect(() => {
+		if (!authSession || !authSession.user) {
+			router.replace("/sign-in");
+		} else {
+			router.replace("/");
+		}
+	}, [authSession]);
 
 	return (
 		<ThemeProvider>
