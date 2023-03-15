8977e172032d058cbb9df56cd3f2d022b3755b23
Pavel Sušický <me@frontendista.cz>
2023-03-15 23:12:18 +0100

    feat: add custom TabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,12 @@
 import { Tabs } from "expo-router";
 import React from "react";
 
+import { TabBar } from "@/components/tab-bar";
+
 export default function DefaultLayout() {
 	return (
 		<Tabs
+			tabBar={TabBar}
 			screenOptions={{
 				headerShown: false
 			}}
