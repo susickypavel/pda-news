669a8ca351805a790a0b97bd561b4ad5daa99201
Pavel Sušický <me@frontendista.cz>
2023-03-16 22:30:16 +0100

    temp

---
+++ app/auth/sign-in.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { SignInForm } from "@/components/signin-form";
+
+export default function SignInScreen() {
+	return <SignInForm />;
+}
---
+++ app/auth/sign-up.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { SignUpForm } from "@/components/signup-form";
+
+export default function SignInScreen() {
+	return <SignUpForm />;
+}
