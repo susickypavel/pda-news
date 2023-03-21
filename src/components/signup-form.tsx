ce8c795ee4d207701c9a8b2c626c6f405bfc25ac
Pavel Sušický <susicky.pavel@protonmail.com>
2023-03-21 10:09:31 +0100

    feat(sign-up): remove email confirmation alert
    Email confirmation is disabled on the backend for now

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -32,8 +32,6 @@ async function onSubmit(data: RegistrationFormData) {
 
 	if (response.error) {
 		Alert.alert("Oops", response.error.message);
-	} else {
-		Alert.alert("Success", "We've sent you an email to verify your account.");
 	}
 }
 
