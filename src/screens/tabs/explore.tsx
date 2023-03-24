54c06a32d941073140a3d5695ffcd43058df7311
Pavel Sušický <me@frontendista.cz>
2023-03-24 21:43:27 +0100

    feat: add basic implementation of scroll indicator

--- src/screens/tabs/explore.tsx
+++ src/screens/tabs/explore.tsx
@@ -1,17 +1,59 @@
-import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { useTheme } from "@rneui/themed";
+import React, { useCallback, useRef, useState } from "react";
+import { Animated, Dimensions, NativeScrollEvent, NativeSyntheticEvent, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ArticlePreview } from "@/components/article-preview";
 
+const { height } = Dimensions.get("window");
+
+const SCROLL_INDICATOR_HEIGHT = 10;
+
 export function ExploreTab() {
+	const { theme } = useTheme();
+	const [scrollIndicatorWidth, setScrollIndicatorWidth] = useState(1);
+	const scrollProgress = useRef(new Animated.Value(0)).current;
+
+	const onScroll = useCallback((e: NativeSyntheticEvent<NativeScrollEvent>) => {
+		const { y } = e.nativeEvent.contentOffset;
+		const contentHeight = e.nativeEvent.contentSize.height;
+		const scrollViewHeight = height - SCROLL_INDICATOR_HEIGHT;
+
+		const newProgress = y / (contentHeight - scrollViewHeight);
+		scrollProgress.setValue(newProgress);
+	}, []);
+
 	return (
 		<SafeAreaView>
-			<ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.container}>
+			<Animated.View
+				onLayout={e => {
+					setScrollIndicatorWidth(-e.nativeEvent.layout.width);
+				}}
+				style={{
+					width: "100%",
+					height: SCROLL_INDICATOR_HEIGHT,
+					backgroundColor: theme.colors.primary,
+					transform: [
+						{
+							translateX: scrollProgress.interpolate({
+								inputRange: [0, 1],
+								outputRange: [scrollIndicatorWidth, 0],
+								extrapolate: "clamp"
+							})
+						}
+					]
+				}}
+			/>
+			<Animated.ScrollView
+				scrollEventThrottle={16}
+				onScroll={onScroll}
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={styles.container}
+			>
 				{Array.from({ length: 10 }).map((_, i) => (
 					<ArticlePreview key={i} />
 				))}
-			</ScrollView>
+			</Animated.ScrollView>
 		</SafeAreaView>
 	);
 }
