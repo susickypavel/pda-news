45640584fa685e574bd5203b1647285f786c1513
Pavel Sušický <me@frontendista.cz>
2023-04-01 10:39:13 +0200

    feat(feed): add infinite scroll

--- src/screens/tabs/news.tsx
+++ src/screens/tabs/news.tsx
@@ -1,36 +1,107 @@
-import React, { useEffect } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { Skeleton, Text } from "@rneui/themed";
+import { FlashList } from "@shopify/flash-list";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useMemo } from "react";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { supabase } from "@/api/supabase";
 import { ArticlePreview } from "@/components/article-preview";
-import { useDailyFeed } from "@/stores/daily-feed";
+
+const PAGE_LIMIT = 5;
+
+async function fetchArticles({ pageParam = 0 }) {
+	const from = pageParam * PAGE_LIMIT;
+	const to = from + PAGE_LIMIT - 1;
+
+	// TODO: Filter by date
+
+	const { data, error } = await supabase
+		.from("articles")
+		.select("id, title, content, source_id (domain), category")
+		.order("published_at", {
+			ascending: false
+		})
+		.range(from, to);
+
+	if (error) {
+		throw new Error(error.message);
+	}
+
+	return data;
+}
+
+const Separator: React.FC = () => <View style={{ height: 32 }} />;
+
+// TODO: Indicator showing infinite scroll that it is loading more items
 
 export const NewsTab: React.FC = () => {
-	const { articles, init } = useDailyFeed();
-
-	useEffect(() => {
-		async function fetchFeed() {
-			await init();
-
-			// TODO: Handle error
+	const { data, isLoading, isError, hasNextPage, isFetchingNextPage, fetchNextPage } = useInfiniteQuery(
+		["daily-feed"],
+		fetchArticles,
+		{
+			getNextPageParam: (_, pages) => Math.floor(pages.flatMap(page => page).length / PAGE_LIMIT)
 		}
+	);
 
-		fetchFeed();
-	}, [init]);
+	// NOTE: This is a premature optimization, I haven't tested real impact on performance.
+	const articles = useMemo(() => data?.pages.flatMap(page => page), [data]);
+
+	const onEndReached = () => {
+		if (hasNextPage && !isFetchingNextPage) {
+			fetchNextPage();
+		}
+	};
+
+	if (isLoading) {
+		// TODO: Improve skeleton
+
+		return (
+			<SafeAreaView style={styles.container} edges={["top"]}>
+				<Skeleton
+					style={{
+						width: "100%",
+						height: 320
+					}}
+				/>
+			</SafeAreaView>
+		);
+	}
+
+	if (isError) {
+		// TODO: Inform user, and retry button (?)
+
+		return (
+			<SafeAreaView style={styles.container} edges={["top"]}>
+				<Text>Error</Text>
+			</SafeAreaView>
+		);
+	}
 
 	return (
-		<SafeAreaView edges={["top"]}>
-			<ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={styles.container}>
-				{articles.map((article, i) => (
-					<ArticlePreview {...article} key={i} />
-				))}
-			</ScrollView>
+		<SafeAreaView style={styles.container} edges={["top"]}>
+			<FlashList
+				data={articles}
+				keyExtractor={item => item.id}
+				// @ts-ignore
+				renderItem={({ item }) => <ArticlePreview {...item} />}
+				ItemSeparatorComponent={Separator}
+				contentContainerStyle={styles.list}
+				estimatedItemSize={200}
+				onEndReached={onEndReached}
+				onEndReachedThreshold={0}
+			/>
 		</SafeAreaView>
 	);
 };
+
 const styles = StyleSheet.create({
 	container: {
-		gap: 32,
+		flex: 1
+	},
+	list: {
 		padding: 8
 	}
 });
+
+NewsTab.displayName = "NewsTab";
