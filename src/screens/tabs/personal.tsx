3965188044fdfed2da55c02f2c663c2c9fe7b2ab
Pavel Sušický <me@frontendista.cz>
2023-04-10 18:12:01 +0200

    fix: settings icon color in dark mode

--- src/screens/tabs/personal.tsx
+++ src/screens/tabs/personal.tsx
@@ -1,15 +1,16 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, Header, Icon } from "@rneui/themed";
+import { Button, Header, Icon, useTheme } from "@rneui/themed";
 import React from "react";
 
 export const PersonalTab: React.FC = () => {
 	const { navigate } = useNavigation();
+	const { theme } = useTheme();
 
 	return (
 		<Header
 			rightComponent={
 				<Button type="clear" onPress={() => navigate("Settings")}>
-					<Icon name="settings" color="black" size={32} />
+					<Icon name="settings" color={theme.colors.black} size={32} />
 				</Button>
 			}
 		/>
