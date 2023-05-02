a7a55b2fc7e88895d61c950faf5a140fb9b57c30
Pavel Sušický <susicky.pavel@protonmail.com>
2023-05-02 10:21:51 +0200

    feat(account): style sign out button

--- src/screens/settings/account.tsx
+++ src/screens/settings/account.tsx
@@ -1,7 +1,10 @@
 import { NavigationProp, RouteProp } from "@react-navigation/native";
+import { Button, Icon } from "@rneui/themed";
 import React from "react";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { RootStackParamList } from "src/types/app";
+
+import { supabase } from "@/api/supabase";
 
 type AccountSettingsScreenRouteProp = RouteProp<RootStackParamList, "AccountSettings">;
 
@@ -13,7 +16,28 @@ type AccountSettingsScreenProps = {
 };
 
 export const AccountSettingsScreen: React.FC<AccountSettingsScreenProps> = () => {
-	return <View />;
+	const styles = StyleSheet.create({
+		container: {
+			padding: 8
+		},
+		signoutButton: {
+			justifyContent: "space-between",
+			paddingHorizontal: 16
+		}
+	});
+
+	return (
+		<View style={styles.container}>
+			<Button
+				buttonStyle={styles.signoutButton}
+				title="Sign out"
+				color="error"
+				onPress={() => supabase.auth.signOut()}
+				icon={<Icon name="logout" type="material" />}
+				iconPosition="right"
+			/>
+		</View>
+	);
 };
 
 AccountSettingsScreen.displayName = "AccountSettingsScreen";
