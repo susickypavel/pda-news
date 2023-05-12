604d66f3a8b93318bf93c5e4702dd6589301088b
Pavel Sušický <me@frontendista.cz>
2023-05-12 12:26:47 +0200

    fix(theming): add correct dark mode colors

--- src/screens/tabs/explore/index.tsx
+++ src/screens/tabs/explore/index.tsx
@@ -3,26 +3,13 @@ import { Button, SearchBar } from "@rneui/base";
 import { useTheme } from "@rneui/themed";
 import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Platform, ScrollView, StyleSheet, Text, View } from "react-native";
 import { CATEGORIES } from "src/constants";
+import { BadgeCategory } from "src/types/theme";
 
 import { supabase } from "@/api/supabase";
 
 import { PreviewRow } from "./preview_row";
-
-// const interests = [
-// 	{ color: "#F0E360", label: "Tech" },
-// 	{ color: "#7DA1F6", label: "Business" },
-// 	{ color: "#58C17B", label: "Climate" },
-// 	{ color: "#DDC0E7", label: "Culture" },
-// 	{ color: "#B68353", label: "Science" },
-// 	{ color: "#F2B040", label: "Sports" },
-// 	{ color: "#F08957", label: "Health" },
-// 	{ color: "#BEAD99", label: "Personal Growth" },
-// 	{ color: "#DF5B4B", label: "World Affairs" },
-// 	{ color: "#A7DEE4", label: "Finance" },
-// 	{ color: "#D2F776", label: "Economy" }
-// ];
 
 export const ExploreTab: React.FC = () => {
 	const [query, setQuery] = useState("");
@@ -39,23 +26,70 @@ export const ExploreTab: React.FC = () => {
 	const { navigate } = useNavigation();
 	const { theme } = useTheme();
 
-	const onPress = (category: string) => {
+	const onPress = (category: BadgeCategory) => {
 		navigate("InterestSubpage", {
 			category
 		});
 	};
 
+	const styles = StyleSheet.create({
+		H2: {
+			color: theme.colors.black,
+			fontSize: 24,
+			fontWeight: "500",
+			marginBottom: 10,
+			textTransform: "capitalize"
+		},
+		container: {
+			flex: 1,
+			paddingTop: 50
+		},
+		interestBtn: {
+			borderRadius: 20,
+			marginRight: 5,
+			paddingLeft: 14,
+			paddingRight: 14
+		},
+		interestTitle: {
+			color: theme.colors.categories.politics.fg,
+			fontSize: 14,
+			textTransform: "capitalize"
+		},
+		interestsContainer: {
+			flex: 1,
+			flexDirection: "row",
+			flexGrow: 0.1,
+			marginBottom: 10,
+			marginLeft: 6,
+			marginTop: 10
+		},
+		personalSelContainer: {
+			flex: 1,
+			flexGrow: 0.1,
+			marginHorizontal: 10,
+			marginTop: 30
+		},
+		searchBar: {
+			backgroundColor: theme.colors.white
+		},
+		text: {
+			color: theme.colors.black,
+			fontSize: 14
+		}
+	});
+
 	return (
 		<View style={styles.container}>
 			<SearchBar
-				platform="ios"
+				containerStyle={styles.searchBar}
+				platform={Platform.OS === "ios" ? "ios" : "android"}
 				onChangeText={newVal => setQuery(newVal)}
 				placeholder="Search articles..."
 				placeholderTextColor="#888"
 				cancelButtonTitle="Cancel"
 				value={query}
 			/>
-			<ScrollView>
+			<ScrollView showsVerticalScrollIndicator={false}>
 				<ScrollView horizontal style={styles.interestsContainer} showsHorizontalScrollIndicator={false}>
 					{CATEGORIES.map(category => (
 						<Button
@@ -87,47 +121,3 @@ export const ExploreTab: React.FC = () => {
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	H2: {
-		color: "black",
-		fontSize: 24,
-		fontWeight: "500",
-		marginBottom: 10,
-		textTransform: "capitalize"
-	},
-	container: {
-		flex: 1,
-		paddingTop: 50
-	},
-	interestBtn: {
-		borderRadius: 20,
-		marginRight: 5,
-		paddingLeft: 14,
-		paddingRight: 14
-	},
-	interestTitle: {
-		color: "#000000",
-		fontSize: 14,
-		textTransform: "capitalize"
-	},
-
-	interestsContainer: {
-		flex: 1,
-		flexDirection: "row",
-		flexGrow: 0.1,
-		marginBottom: 10,
-		marginLeft: 6,
-		marginTop: 10
-	},
-	personalSelContainer: {
-		flex: 1,
-		flexGrow: 0.1,
-		marginHorizontal: 10,
-		marginTop: 30
-	},
-	text: {
-		color: "black",
-		fontSize: 14
-	}
-});
--- src/screens/tabs/explore/preview_card.tsx
+++ src/screens/tabs/explore/preview_card.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { Badge, Image } from "@rneui/themed";
+import { Badge, Image, useTheme } from "@rneui/themed";
 import React from "react";
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 
@@ -8,6 +8,37 @@ import { ArticlePreviewProps } from "@/components/article-preview";
 export const PreviewCard: React.FC<ArticlePreviewProps> = props => {
 	const { title, category } = props;
 	const { navigate } = useNavigation();
+	const { theme } = useTheme();
+
+	const styles = StyleSheet.create({
+		author: {
+			alignItems: "center",
+			flexDirection: "row",
+			gap: 8,
+			marginVertical: 8
+		},
+		container: {
+			flex: 1,
+			height: 260,
+			marginRight: 8,
+			marginVertical: 12,
+			width: 160
+		},
+		descriptionContainer: {
+			marginLeft: 6
+		},
+		thumbnailImg: {
+			borderRadius: 4,
+			height: 160,
+			width: 160
+		},
+		title: {
+			color: theme.colors.black,
+			fontSize: 16,
+			fontWeight: "500",
+			lineHeight: 18
+		}
+	});
 
 	return (
 		<TouchableWithoutFeedback
@@ -33,32 +64,3 @@ export const PreviewCard: React.FC<ArticlePreviewProps> = props => {
 };
 
 PreviewCard.displayName = "ArticlePreview";
-
-const styles = StyleSheet.create({
-	author: {
-		alignItems: "center",
-		flexDirection: "row",
-		gap: 8,
-		marginVertical: 8
-	},
-	container: {
-		flex: 1,
-		height: 260,
-		marginRight: 8,
-		marginVertical: 12,
-		width: 160
-	},
-	descriptionContainer: {
-		marginLeft: 6
-	},
-	thumbnailImg: {
-		borderRadius: 4,
-		height: 160,
-		width: 160
-	},
-	title: {
-		fontSize: 16,
-		fontWeight: "500",
-		lineHeight: 18
-	}
-});
