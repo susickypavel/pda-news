57994f825f52cfc1aa5d4a2ba48a4a046e6f74f5
Matouš Najman <matous@najman.cz>
2023-05-12 21:39:54 +0200

    remove mock data

--- src/screens/tabs/personal/personal.tsx
+++ src/screens/tabs/personal/personal.tsx
@@ -9,32 +9,6 @@ import { ArticlePreviewProps } from "@/components/article-preview";
 
 import CustomBackdrop from "./customs-for-bottomsheet/custom-backdrop";
 
-const articles: ArticlePreviewProps[] = [
-	{
-		date: "12.05.2023",
-		category: "science",
-		title: "Pozor na falešné weby napodobující státní instituce. Působí velmi přesvědčivě, naletět je snadné",
-		source_id: { name: "irozhlas" },
-		content: ""
-	},
-	{
-		date: "11.05.2023",
-		category: "sports",
-		title: "První evropská dražba kostry pravěkého masožravce. Tyranosaurus rex se prodal za 131 milionů korun",
-		source_id: { name: "irozhlas" },
-		content:
-			"Kostra dinosaura Tyrannosaurus rex se v úterý v dražbě ve Švýcarsku prodala za celkovou částku 5,5 milionu franků (131 milionů korun), informovala aukční síň Koller. Nový majitel skeletu je z Evropy, další podrobnosti o něm známy nejsou. Šlo o první dražbu kostry prehistorického masožravého živočicha v Evropě, předchozí se konaly ve Spojených státech. Curych 21:01 18. dubna 2023 Sdílet na Facebooku Sdílet na Twitteru Sdílet na LinkedIn Tisknout Kopírovat url adresu Zkrácená adresa Kopírovat do schránky Zavřít Agentura DPA píše, že pozůstatky těchto dinosaurů patří na aukcích k nejvyhledávanějším kouskům, protože Tyrannosaurus rex byl dlouho považován za největšího predátora, jaký kdy obýval Zemi. Mezitím se sice našly pozůstatky ještě větších dinosaurů, což ovšem T. rexovi na exkluzivitě neubírá. Doposud se v aukcích prodaly jen tři kostry těchto živočichů. V Británii byl vydražen Kupkův obraz za skoro 123 milionů korun. Pocházel z dědictví herce Conneryho Číst článek V úterý prodaná kostra nese název Trinity, protože je seskládaná z kostí tří různých zvířat, která byla v letech 2008 až 2013 objeveny v amerických státech Montana a Wyoming. Původní kostní materiál tvoří polovinu skeletu. Společnost Koller uvedla, že mimořádně vzácná a pozoruhodně dobře zachovalá je lebka pradávného zvířete. Kostra na délku měří 11,6 metru, na výšku 3,9 metru. Podle agentury AP částka, za niž se kostra prodala, zaostala za očekáváním. Karl Green, šéf marketingu aukční síně Koller, řekl, že možná kvůli tomu, že Trinity je „slepencem“ tří dinosaurů, nepřitáhla dražba „puristy“, tedy původní, neskládané kostry. „Jde o dobrou cenu za dinosaura. Doufám, že bude vystaven někde na veřejnosti,“ dodal. Tyrannosaurus rex žil před 68 až 66 miliony let na území dnešních Spojených států. Podle posledních výzkumů mohl tento obávaný prehistorický predátor dosáhnout hmotnosti až 15 tun. ČTK Sdílet na Facebooku Sdílet na Twitteru Sdílet na LinkedIn Tisknout Kopírovat url adresu Zkrácená adresa Kopírovat do schránky Zavřít"
-	},
-	{
-		date: "14.05.2023",
-		category: "technology",
-		title: "První evropská dražba kostry pravěkého masožravce. Tyranosaurus rex se prodal za 131 milionů koru",
-		source_id: { name: "irozhlas" },
-		content:
-			"Kostra dinosaura Tyrannosaurus rex se v úterý v dražbě ve Švýcarsku prodala za celkovou částku 5,5 milionu franků (131 milionů korun), informovala aukční síň Koller. Nový majitel skeletu je z Evropy, další podrobnosti o něm známy nejsou. Šlo o první dražbu kostry prehistorického masožravého živočicha v Evropě, předchozí se konaly ve Spojených státech. Curych 21:01 18. dubna 2023 Sdílet na Facebooku Sdílet na Twitteru Sdílet na LinkedIn Tisknout Kopírovat url adresu Zkrácená adresa Kopírovat do schránky Zavřít Agentura DPA píše, že pozůstatky těchto dinosaurů patří na aukcích k nejvyhledávanějším kouskům, protože Tyrannosaurus rex byl dlouho považován za největšího predátora, jaký kdy obýval Zemi. Mezitím se sice našly pozůstatky ještě větších dinosaurů, což ovšem T. rexovi na exkluzivitě neubírá. Doposud se v aukcích prodaly jen tři kostry těchto živočichů. V Británii byl vydražen Kupkův obraz za skoro 123 milionů korun. Pocházel z dědictví herce Conneryho Číst článek V úterý prodaná kostra nese název Trinity, protože je seskládaná z kostí tří různých zvířat, která byla v letech 2008 až 2013 objeveny v amerických státech Montana a Wyoming. Původní kostní materiál tvoří polovinu skeletu. Společnost Koller uvedla, že mimořádně vzácná a pozoruhodně dobře zachovalá je lebka pradávného zvířete. Kostra na délku měří 11,6 metru, na výšku 3,9 metru. Podle agentury AP částka, za niž se kostra prodala, zaostala za očekáváním. Karl Green, šéf marketingu aukční síně Koller, řekl, že možná kvůli tomu, že Trinity je „slepencem“ tří dinosaurů, nepřitáhla dražba „puristy“, tedy původní, neskládané kostry. „Jde o dobrou cenu za dinosaura. Doufám, že bude vystaven někde na veřejnosti,“ dodal. Tyrannosaurus rex žil před 68 až 66 miliony let na území dnešních Spojených států. Podle posledních výzkumů mohl tento obávaný prehistorický predátor dosáhnout hmotnosti až 15 tun. ČTK Sdílet na Facebooku Sdílet na Twitteru Sdílet na LinkedIn Tisknout Kopírovat url adresu Zkrácená adresa Kopírovat do schránky Zavřít"
-	}
-];
-
 export const PersonalTab: React.FC = () => {
 	const { navigate } = useNavigation();
 	const { theme } = useTheme();
@@ -54,6 +28,9 @@ export const PersonalTab: React.FC = () => {
 	useEffect(() => {
 		sortArticles(sortedArticles, selectedFilter);
 	}, [selectedFilter]);
+
+	// jenom temporary
+	const articles: ArticlePreviewProps[] = [];
 
 	const sortedArticles = sortArticles(articles, "alphabetical");
 
