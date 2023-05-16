a53c2949923f11435b6cc2f778ee30c9fc93d0b6
Pavel Sušický <me@frontendista.cz>
2023-05-16 14:50:05 +0200

    refactor: TabBar component

---
+++ src/components/main-tab-bar.tsx
@@ -0,0 +1,74 @@
+import { Icon, Text, useTheme } from "@rneui/themed";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { type NavigationState, type SceneRendererProps, TabBar, TabBarItem } from "react-native-tab-view";
+import type { Props } from "react-native-tab-view/lib/typescript/src/TabBarItem";
+
+type MainTabBarProps = SceneRendererProps & {
+	navigationState: NavigationState<RouteData>;
+};
+
+type IconData = {
+	[key: string]: {
+		type: "ionicon";
+		name: string;
+		nameActive: string;
+	};
+};
+
+const icons = {
+	news: { type: "ionicon", name: "ios-newspaper-outline", nameActive: "ios-newspaper" },
+	explore: { type: "ionicon", name: "compass-outline", nameActive: "compass" },
+	personal: { type: "ionicon", name: "bookmarks-outline", nameActive: "bookmarks" }
+} as const satisfies IconData;
+
+export type RouteData = {
+	key: keyof typeof icons;
+	title: string;
+};
+
+type MainTabBarItemProps = Props<RouteData> & {
+	key: string;
+};
+
+const MainTabBarItem: React.FC<MainTabBarItemProps> = props => {
+	return (
+		<TabBarItem
+			{...props}
+			renderLabel={({ color }) => (
+				<Text style={{ color, fontFamily: "InterTightSemiBold" }}>{props.route.title}</Text>
+			)}
+			renderIcon={({ color, focused }) => (
+				<Icon
+					style={{
+						marginBottom: 8
+					}}
+					iconStyle={{ color }}
+					type={icons[props.route.key].type}
+					name={focused ? icons[props.route.key].nameActive : icons[props.route.key].name}
+				/>
+			)}
+		/>
+	);
+};
+
+export const MainTabBar: React.FC<MainTabBarProps> = props => {
+	const { theme } = useTheme();
+
+	return (
+		<SafeAreaView edges={["bottom"]}>
+			<TabBar
+				activeColor={theme.colors.black}
+				inactiveColor={theme.colors.grey3}
+				renderIndicator={() => null}
+				renderTabBarItem={props => <MainTabBarItem {...props} />}
+				style={{
+					backgroundColor: theme.colors.background
+				}}
+				{...props}
+			/>
+		</SafeAreaView>
+	);
+};
+
+MainTabBar.displayName = "MainTabBar";
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,10 +1,9 @@
 import { NavigationProp, RouteProp } from "@react-navigation/native";
-import { Icon, Text, useTheme } from "@rneui/themed";
 import React, { useState } from "react";
 import { useWindowDimensions } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { SceneMap, TabBar, TabBarItem, TabView } from "react-native-tab-view";
+import { SceneMap, TabView } from "react-native-tab-view";
 
+import { MainTabBar, RouteData } from "@/components/main-tab-bar";
 import type { RootStackParamList } from "@/types/app";
 
 import { ExploreTab } from "./tabs/explore";
@@ -26,83 +25,25 @@ type PersonalScreenProps = {
 	navigation: PersonalScreenNavigationProp;
 };
 
-type IconData = {
-	[key: string]: {
-		type: "ionicon";
-		name: string;
-		nameActive: string;
-	};
-};
-
-const icons = {
-	news: { type: "ionicon", name: "ios-newspaper-outline", nameActive: "ios-newspaper" },
-	explore: { type: "ionicon", name: "compass-outline", nameActive: "compass" },
-	personal: { type: "ionicon", name: "person-outline", nameActive: "person" }
-} satisfies IconData;
-
-type RouteData = {
-	key: keyof typeof icons;
-	title: string;
-};
+const routes: RouteData[] = [
+	{ key: "news", title: "News" },
+	{ key: "explore", title: "Explore" },
+	{ key: "personal", title: "Saved" }
+];
 
 export const HomeScreen: React.FC<PersonalScreenProps> = () => {
-	const layout = useWindowDimensions();
-	const { theme } = useTheme();
-
+	const { width } = useWindowDimensions();
 	const [index, setIndex] = useState(0);
-	const [routes] = useState<RouteData[]>([
-		{ key: "news", title: "News" },
-		{ key: "explore", title: "Explore" },
-		{ key: "personal", title: "Personal" }
-	]);
 
 	return (
 		<TabView
+			initialLayout={{ width }}
 			navigationState={{ index, routes }}
 			swipeEnabled={false}
 			renderScene={renderScene}
 			onIndexChange={setIndex}
-			initialLayout={{ width: layout.width }}
 			tabBarPosition="bottom"
-			renderTabBar={props => (
-				<SafeAreaView edges={["bottom"]}>
-					<TabBar
-						renderIndicator={() => null}
-						activeColor={theme.colors.black}
-						inactiveColor={theme.colors.grey3}
-						renderTabBarItem={props => {
-							return (
-								<TabBarItem
-									{...props}
-									renderLabel={({ color }) => (
-										<Text style={{ color, fontFamily: "InterTightSemiBold" }}>
-											{props.route.title}
-										</Text>
-									)}
-									renderIcon={({ color, focused }) => (
-										<Icon
-											style={{
-												marginBottom: 8
-											}}
-											iconStyle={{ color }}
-											type={icons[props.route.key].type}
-											name={
-												focused
-													? icons[props.route.key].nameActive
-													: icons[props.route.key].name
-											}
-										/>
-									)}
-								/>
-							);
-						}}
-						style={{
-							backgroundColor: theme.colors.background
-						}}
-						{...props}
-					/>
-				</SafeAreaView>
-			)}
+			renderTabBar={props => <MainTabBar {...props} />}
 		/>
 	);
 };
