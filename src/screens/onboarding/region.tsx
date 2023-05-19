77026a193b4ee7a7cb9c6cbfb8b707d80215e3cb
Pavel Sušický <me@frontendista.cz>
2023-05-19 16:45:58 +0200

    feat(onboarding): extended object with home region

--- src/screens/onboarding/region.tsx
+++ src/screens/onboarding/region.tsx
@@ -1,26 +1,53 @@
-import { useTheme } from "@rneui/themed";
+import { Picker } from "@react-native-picker/picker";
+import { Text, useTheme } from "@rneui/themed";
 import React, { PropsWithChildren } from "react";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { OnboardingFooter } from "@/components/onboarding-footer";
+import { useOnboarding } from "@/context/onboarding";
 
 type OnboardingRegionProps = PropsWithChildren<unknown>;
 
 export const OnboardingRegionScreen: React.FC<OnboardingRegionProps> = () => {
 	const { theme } = useTheme();
+	const { homeRegion, setHomeRegion } = useOnboarding();
 
 	const styles = StyleSheet.create({
 		container: {
 			backgroundColor: theme.colors.brandAlternative,
 			flex: 1,
 			paddingVertical: theme.spacing.xl
+		},
+		content: {
+			flex: 1,
+			justifyContent: "center",
+			paddingVertical: theme.spacing.sm
+		},
+		title: {
+			fontFamily: "InterTightBold",
+			fontSize: 24,
+			textAlign: "center"
 		}
 	});
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<View style={{ flex: 1 }} />
+			<Text style={styles.title}>Select your home region</Text>
+			<View style={styles.content}>
+				<Picker
+					style={{
+						backgroundColor: theme.colors.background
+					}}
+					selectionColor={theme.colors.primary}
+					selectedValue={homeRegion}
+					onValueChange={setHomeRegion}
+				>
+					<Picker.Item label="Czech Republic" value="cz" />
+					<Picker.Item label="Germany" value="de" />
+					<Picker.Item label="Great Britain" value="gb" />
+				</Picker>
+			</View>
 			<OnboardingFooter />
 		</SafeAreaView>
 	);
